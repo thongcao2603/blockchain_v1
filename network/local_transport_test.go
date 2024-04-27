edff5403a729e4c92ab6af67cf66e1e07cb41726
thongcao2603 <133794541+thongcao2603@users.noreply.github.com>
2024-04-27 07:53:10 +0700

    test connect, sendmessage between 2 transport

--- network/local_transport_test.go
+++ network/local_transport_test.go
@@ -1,1 +1,30 @@
 package network
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConnect(t *testing.T) {
+	tra := NewLocalTransport("LOCAL")
+	trb := NewLocalTransport("REMOTE")
+
+	trb.Connect(tra)
+
+	assert.Equal(t, trb.peers[tra.Addr()], tra)
+
+}
+
+func TestSendMessage(t *testing.T) {
+	tra := NewLocalTransport("LOCAL")
+	trb := NewLocalTransport("REMOTE")
+
+	trb.Connect(tra)
+
+	msg := []byte("hello world")
+	assert.Nil(t, trb.SendMessage(tra.Addr(), msg))
+
+	rpc := <-tra.Consume()
+
+	assert.Equal(t, rpc.Payload, msg)
+}
